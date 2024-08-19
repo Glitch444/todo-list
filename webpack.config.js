3d4b7c194ffda35b070ee87ef790e005faf5dc87
Abaz <Abaz95@live.co.uk>
2024-08-19 19:44:10 +0100

    Create main files and set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+
+  mode: 'development',
+  
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Apply this rule to .css files
+        use: ['style-loader', 'css-loader'], // Use these loaders for .css files
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Apply this rule to image files
+        type: 'asset/resource', // Use the file-loader to process image files
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, // Apply this rule to font files
+        type: 'asset/resource', // Use the file-loader to process font files
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
