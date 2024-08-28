9ca31997c175ef45ae804ccb39e56560be2e63e1
Abaz <Abaz95@live.co.uk>
2024-08-28 14:57:35 +0100

    testing deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,34 +14,40 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/, 
-        use: [
-          MiniCssExtractPlugin.loader,
-          , 'css-loader',
-        ], 
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, 
-        type: 'asset/resource', 
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i, 
-        type: 'asset/resource', 
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
 
   plugins: [
+    new CleanWebpackPlugin(), // Cleans the dist folder before each build
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
-
   ],
-  
+
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
@@ -50,4 +57,6 @@ module.exports = {
   },
 
   mode: 'development',
+
+  devtool: 'source-map', // Enables source maps for easier debugging
 };
